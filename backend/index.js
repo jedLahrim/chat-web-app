e26b092df0d35f1f6a7b938347aa2e65ab218417
jed <jolix123@gmail.com>
2024-02-25 20:52:34 +0100

    first commit

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ router.get('', (req, res) => {
 })
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
