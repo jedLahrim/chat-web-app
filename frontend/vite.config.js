bf36919e7f7eb9e832faa107b5111a3ae73fc72b
jed <jolix123@gmail.com>
2024-02-25 20:57:45 +0100

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: "http://localhost:5002",
+                target: "https://chat-web-app-backend.vercel.app",
             },
         },
     },
